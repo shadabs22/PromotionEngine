a98aa33b27c3e65f5bde1c7dee775bd9106897cd
Shaikh Mohd Shadab <shadabs22@gmail.com>
2020-06-15 00:29:07 +0530

    Promotion 2 logic
    Promotion 2 logic

---
+++ PromotionEngineDAL/PromotioonCalculator/Promotion2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PromotionEngineDAL.PromotioonCalculator
+{
+    public class Promotion2 : IPromotionCalculation
+    {
+        public List<SkuToOrder> CalculatePromotion(List<SkuToOrder> list)
+        {
+            if (list.Where(f => f.SKUID == 2 && f.Quantity > 0).Count() > 0)
+            {
+                var sku = list.Where(f => f.SKUID == 2 && f.Quantity > 0).First();
+                if (sku.Quantity > 1)
+                {
+                    int quotient = sku.Quantity / 2;
+                    int remainder = sku.Quantity % 2;
+                    int index = list.IndexOf(sku);
+                    sku.Price = (quotient * 45) + ((remainder) * 30);
+                    list[index].Price = sku.Price;
+                    //return (quotient * 45) + ((remainder) * 30);
+                }
+            }
+            return list;
+        }
+    }
+}
