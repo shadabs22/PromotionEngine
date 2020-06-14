47cad2f7f562a68bee3f279eea752e5b75607671
Shaikh Mohd Shadab <shadabs22@gmail.com>
2020-06-15 00:44:01 +0530

    final clubbing of promotions

---
+++ PromotionEngineDAL/PromotioonCalculator/FinalPromotion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PromotionEngineDAL.PromotioonCalculator
+{
+    public class FinalPromotion
+    {
+        public List<SkuToOrder> CalculatePromotion(List<SkuToOrder> list)
+        {
+            if (list.Count() > 0)
+            {
+                list = new Promotion1().CalculatePromotion(list);
+                list = new Promotion2().CalculatePromotion(list);
+                list = new Promotion3().CalculatePromotion(list);
+            }
+            return list;
+        }
+    }
+}
