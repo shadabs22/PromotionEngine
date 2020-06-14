f3d9cac893984924210a6d5583f79e2df97eae68
Shaikh Mohd Shadab <shadabs22@gmail.com>
2020-06-15 00:34:20 +0530

    Promotion 3 logic

---
+++ PromotionEngineDAL/PromotioonCalculator/Promotion3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PromotionEngineDAL.PromotioonCalculator
+{
+    public class Promotion3 : IPromotionCalculation
+    {
+        public List<SkuToOrder> CalculatePromotion(List<SkuToOrder> list)
+        {
+            if(list.Where(f => f.SKUID == 3 && f.Quantity > 0).Count()>0 && list.Where(f => f.SKUID == 4 && f.Quantity > 0).Count() > 0)
+            {
+                var skuC = list.Where(f => f.SKUID == 3).First();
+                int skuCIndex = list.IndexOf(skuC);
+                var skuD = list.Where(f => f.SKUID == 4).First();
+                int skuDIndex = list.IndexOf(skuD);
+                if (skuC.Quantity > 0 && skuD.Quantity > 0)
+                {
+                    if (skuC.Quantity == skuD.Quantity)
+                    {
+                        skuC.Price =0;
+                        skuD.Price = skuD.Quantity * 30;
+                    }
+                    else if(skuC.Quantity > skuD.Quantity)
+                    {
+                        int difference = skuC.Quantity - skuD.Quantity;
+                        skuC.Price = difference * 20;
+                        skuD.Price = skuD.Quantity * 30;
+                    }
+                    else if (skuC.Quantity < skuD.Quantity)
+                    {
+                        int difference = skuD.Quantity - skuC.Quantity;
+                        skuC.Price = 0;
+                        skuD.Price = (skuC.Quantity * 30) + (difference * 15);
+                    }
+                }
+            }
+            return list;
+        }
+    }
+}
